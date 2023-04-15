8ca126b5f08d24d781d74aa22fd36167e6903b18
nhatkytran <nhatky.tran.2002@gmail.com>
2023-04-15 14:12:45 +0700

    Asynchronous and Event Loop

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
 'use strict';
+
+console.log('Start');
+
+const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+
+console.log(res);
+
+const data = await res.json();
+
+console.log(data);
